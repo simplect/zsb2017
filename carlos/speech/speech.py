1eacc0348b4764a5d8629bf06107f4940927142f
Alexandra Spruit <alexandra.spruit@hotmail.com>
2017-06-23 12:16:59 +0200

    sudokuNao class updated

--- carlos/speech/speech.py
+++ carlos/speech/speech.py
@@ -81,6 +81,7 @@ class SudokuNao:
         sudokuArray = []
         for n in range(9):
             substr = sudokuStr[(n*9):(n+1*9)]
+            print(substr)
             rowArray = []
             for x in substr:
                 rowArray.append(x)
