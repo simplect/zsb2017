8c84af91a762f4c89df8e54503bc33fcfcefcd23
HannahMin <hannahmmin@gmail.com>
2017-06-29 12:28:22 +0200

    update speech class

--- carlos/speech/speech.py
+++ carlos/speech/speech.py
@@ -13,92 +13,74 @@ class Speech:
         self.tts.setVolume(0.8)
 
     # outputs the intro monologuew
-    def introSpeech(self):
-        self.animated_speech.say("^start(animations/Stand/Gestures/You_1) Hey {}! Do you want to make a sudoku with me? You can press on my feet to answer my questions.".format(self.current_name))
+    def intro_speech(self):
+        randNum = randint(0,2)
+        if randNum == 0:
+            self.animated_speech.say("^start(animations/Stand/Gestures/You_1) Hey {}! Do you want to make a sudoku with me? You can touch my feet to answer my questions.".format(self.current_name))
+        if randNum == 1:
+            self.animated_speech.say("^start(animations/Stand/Gestures/You_1) Hello {}! Do you want to make a sudoku with me? Press on my feet to answer my questions.".format(self.current_name))
+        if randNum == 2:
+            self.animated_speech.say("^start(animations/Stand/Gestures/You_1) Hi {}! Would you like to make a sudoku with me? You can press on my feet to answer my questions.".format(self.current_name))
 
-    def askForRules(self):
+    def ask_for_rules(self):
         self.animated_speech.say("^start(animations/Stand/Gestures/Explain_2) Would you like to hear the rules?")
 
-    def askForSudoku(self):
+    def ask_for_sudoku(self):
         self.animated_speech.say("^start(animations/Stand/Gestures/Explain_1) Let me see your sudoku puzzle before we start. ^stop(animations/Stand/Gestures/Explain_1)")
 
-    def askForCheck(self):
-        self.animated_speech.say("^start(animations/Stand/Gestures/Explain_1) Write it down and show it to me, please! I can check it for you!")
+    def say_write_down(self):
+        self.animated_speech.say("^start(animations/Stand/Gestures/Explain_1) Write it down and show it to me.")
 
     def bye(self):
         self.animated_speech.say("^start(animations/Stand/Gestures/You_2) Bye {}! See you soon!.".format(self.current_name))
 
-    def wrongAnswerGetHint(self, sudoku):
-        self.animated_speech.say("^start(animations/Stand/Emotions/Neutral/Embarrassed_1) Oops! I think you made a mistake. Do you want a hint?")
-        while(True):
-            self.giveHint(sudoku)
-            randNum = randint(1,2)
-            if randNum == 1:
-                self.getRandomHint()
-            elif randNum == 2:
-                sudokuNao = SudokuNao(sudoku)
-                digit = sudokuNao.checkDigit(sudoku)
-                self.checkThisDigit(digit)
-            self.askForSquare(False, False)
-            # TODO, FIX THIS
-            return
-            if saysYes():
-                pass
-                # if correctAnswer():
-                    # self.rightAnswer()
-                    #break
-                # else:
-                    #continue
-            else:
-                continue
+    def wrong_answer_get_hint(self, sudoku):
+        self.animated_speech.say("^start(animations/Stand/Emotions/Neutral/Embarrassed_1) Oops! I think you made a mistake.")
+        self.giveHint(sudoku)
 
-    def rightAnswer(self):
+
+    def right_answer(self):
         self.animated_speech.say("^start(animations/Stand/Gestures/Yes_1) Well done!")
 
-    def askForSquare(self, begin, end):
-        if begin == True:
-            self.seenSudoku()
-            begin == False
-        elif end == True:
-            self.lastSquare()
+    def ask_for_square(self):
+        if self.countZeros(sudoku) == 1:
+            self.animated_speech.say("^start(animations/Stand/Gestures/Enthusiastic_3) Only one square to go, do you know the answer?")
         else:
             self.animated_speech.say("^start(animations/Stand/Gestures/Enthusiastic_5) Can you fill in another square?")
 
-    def notFilledAnythingIn(self):
+    def say_no_answer(self):
         self.animated_speech.say("^start(animations/Stand/Emotions/Neutral/Embarrassed_1) You didn't fill in anything. Please, do so now.")
 
-    def giveHint(self, sudoku):
+    def give_hint(self, sudoku):
         self.animated_speech.say("^start(animations/Stand/Gestures/ShowSky_1) Let me give you a hint.")
         randNum = randint(1,2)
         if randNum == 1:
             self.getRandomHint()
         elif randNum == 2:
             sudokuNao = SudokuNao(([],[]))
-            digit = sudokuNao.checkDigit(sudoku)
-            self.checkThisDigit(digit)
-
-    def lastSquare(self):
-        self.animated_speech.say("^start(animations/Stand/Gestures/Enthusiastic_3) Only one box to go!")
-
-    # outputs all the functions the nao can provide to the player
-    def instructionMenu(self):
-        self.tts.say("There are a few things I can do for you.")
-        self.tts.say("I can give you general sudoku solving techniques or I can give you a general hint.")
-        self.tts.say("Furthermore, you can ask me for a digit in a cell and I will give it to you.")
-        self.tts.say("Lastly, I can give you the full answer of the sudoku")
+            digit = sudokuNao.best_number(sudoku)
+            self.look_for_number(digit)
 
     # nao confirms it has seen the sudoku
-    def seenSudoku(self):
-        self.animated_speech.say("^start(animations/Stand/Gestures/Hey_1) Nice one! Can you fill in a square already?")
+    def seen_sudoku(self):
+        randNum = randint(0,3)
+        if randNum == 0:
+            self.animated_speech.say("^start(animations/Stand/Gestures/Hey_1) Nice one! Can you fill in a square?")
+        if randNum == 1:
+            self.animated_speech.say("^start(animations/Stand/Gestures/Hey_1) Thank you! Can you fill in a square?")
+        if randNum == 2:
+            self.animated_speech.say("^start(animations/Stand/Gestures/Hey_1) That's great! Can you fill in a square?")
+        if randNum == 3:
+            self.animated_speech.say("^start(animations/Stand/Gestures/Hey_1) Very nice! Can you fill in a square?")
 
     # outputs the general rules of the sudoku game
-    def getGameRules(self):
+    def get_game_rules(self):
         self.animated_speech.say("^start(animations/Stand/Gestures/Explain_7) To complete your sudoku correctly, you must fill in al the empty squares.")
         self.animated_speech.say("^start(animations/Stand/Gestures/Explain_8) When you are finished, the numbers 1 to 9 must appear exactly once in each row, column and box.")
 
     # gives one of the four random hints
-    def getRandomHint(self):
-        randNum = randint(0,4)
+    def get_random_hint(self):
+        randNum = randint(0,3)
         if randNum == 0:
             self.animated_speech.say("^start(animations/Stand/Gestures/YouKnowWhat_1) Have you already looked at the most filled in row, column or box?")
 
@@ -111,16 +93,8 @@ class Speech:
         elif randNum == 3:
             self.animated_speech.say("^start(animations/Stand/Gestures/YouKnowWhat_5) Look at three boxes and see if there are two identical numbers in two of the boxes. Maybe you can fill in this number in the third box?")
 
-
-    # gets a sudoku and a tuple with coordinates of a square
-    # outputs the digit on that square
-    def getHint(self, sudoku, coordinates):
-        row,col = coordinates
-        digit = sudoku[row][col]
-        self.animated_speech.say("^start(animations/Stand/Gestures/Explain_3) The digit on that cell is "+str(digit))
-
     # reads aloud the given sudoku
-    def readSudoku(self, sudoku):
+    def read_sudoku(self, sudoku):
         self.animated_speech.say("^start(animations/Stand/Gestures/Me_1) Okay. I will now read all the digits from the top left corner down to the bottom right corner.")
         for row in range(9):
             for col in range(9):
@@ -130,68 +104,19 @@ class Speech:
                 else:
                     self.tts.say(str(digit))
 
-    # set the new volume (type double) in range [0-1]
-    def setVolume(self, volume):
-        if volume >= 0 and volume <=1.0:
-            self.tts.setVolume(volume);
-        else:
-            self.tts.say("My volume could not be changed correctly.");
-
     # tells the player to look at a given number
-    def checkThisDigit(self, digit):
-        self.tts.say("Here's a tip: maybe you can fill out number"+ str(digit));
-
-    # asks if the player is sure of his question
-    def checkIfSure(self):
-        self.tts.say("Are you sure you want me to tell you?");
-
-    # tells player the nao didn't understand the message
-    def didNotUnderstand(self):
-        self.tts.say("Sorry, I didn't understand");
-
-    # says okay
-    def okay(self):
-        self.tts.say("Okay.");
-
-
-class Dialoge:
-    def __init__(self, IP, PORT):
-        self.speech = Speech(IP, PORT)
-
-    # chooses the right response according to the audio input
-    def chooseRightAnswer(self, sudoku, sentence, row, col, volume):
-        if sentence == "give random hint":
-            self.speech.getRandomHint()
-        elif sentence == "give instructions" or sentence == "what can you help me with":
-            self.speech.instructionMenu()
-        elif sentence == "rules":
-            self.speech.getGameRules()
-        elif sentence == "give hint":
-            digit = SudokuNao.checkDigit(sudoku)
-            self.speech.checkThisDigit(digit)
-        elif sentence == "full answer":
-            self.speech.checkIfSure()
-            # if speechRecognition.checkIfSure() == True:
-            self.speech.readSudoku(sudoku)
-            # else:
-            # self.speech.okay()
-        elif row != None and col != None:
-            self.speech.getHint(sudoku, (row, col))
-        elif volume != None:
-            self.speech.setVolume(volume)
-        else:
-            self.speech.didNotUnderstand()
-
+    def look_for_number(self, digit):
+        self.tts.say("Here's a tip: maybe you can fill out number"+ str(digit))
 
 class SudokuNao:
     def __init__(self, strings):
         self.sudoku = self.makeSudokuArray(strings[0])
         self.sudokuAnswer = self.makeSudokuArray(strings[1])
 
-    def updateSudoku(self, string):
+    def update_sudoku(self, string):
         self.sudoku = self.makeSudokuArray(string)
 
-    def answerIsCorrect(self):
+    def is_correct(self):
         for x in range(9):
             for y in range(9):
                 if self.sudoku[x][y] == 0:
@@ -200,7 +125,7 @@ class SudokuNao:
                     return False
         return True
 
-    def printArrays(self):
+    def print_arrays(self):
         print("The start sudoku is: ")
         for x in self.sudoku:
             print(x)
@@ -208,7 +133,7 @@ class SudokuNao:
         for x in self.sudokuAnswer:
             print(x)
 
-    def checkDigit(self, sudoku):
+    def best_number(self, sudoku):
         best_count = 0
         number = 0
         for i in range(1,10):
@@ -227,7 +152,7 @@ class SudokuNao:
                     count += 1
         return count
 
-    def makeSudokuArray(self, sudokuStr):
+    def make_sudoku_array(self, sudokuStr):
         sudokuArray = []
         for n in range(9):
             substr = sudokuStr[(n*9):((n+1)*9)]
@@ -237,7 +162,7 @@ class SudokuNao:
             sudokuArray.append(rowArray)
         return sudokuArray
 
-    def countZeros(self, sudoku):
+    def count_zeros(self, sudoku):
         # Works with a string for now
         numZeros = 0
         for x in range(9):
@@ -246,7 +171,7 @@ class SudokuNao:
                     numZeros += 1
         return numZeros
 
-    def checkIfEnd(self, sudoku):
-        if self.countZeros(sudoku) == 1:
+    def check_if_end(self, sudoku):
+        if self.countZeros(sudoku) == 0:
             return True
         return False
