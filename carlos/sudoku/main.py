8c6da8a8fa7e4c5d34a85eb361c743e00c58e1c9
Merijn Testroote <merijn@studentvooralles.nl>
2017-06-30 13:27:42 +0200

    Added code headers

--- carlos/sudoku/main.py
+++ carlos/sudoku/main.py
@@ -12,6 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# main.py Code edited by:
+
+# Merijn Testroote, 11173106
+# Babette Mooij, 10740414
+# Alexandra Spruit, 11262273
+# Hannah Min, 11011580
+
+# This code has been developed for the course ZSB 2017
+# at the University of Amsterdam
+
+# 30-06-2017 - We take no responsibility for the 
+# effects of this software nor if Carlos is taking over the
+# world or getting around in fancy cars and winning all poker games.
+# The code is served as is.
+
 import cv2
 from sudoku.image_parser import SudokuImageParser, ImageError
 from sudoku.solver import SudokuSolver, ContradictionError
